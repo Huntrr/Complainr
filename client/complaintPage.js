d316bdc69afcbf4fddb153c3825d18a5c465542b
AashilDesai <aashild@seas.upenn.edu>
2015-09-05 18:38:39 -0400

    Formatting Resolutions
    Better formatted the resolutions page.
    CSS is cool

--- client/complaintPage.js
+++ client/complaintPage.js
@@ -2,7 +2,25 @@ Template.complaintPage.helpers({
   
   resolutionList: function() {
   return Resolutions.find({complaintId: this.complaints._id})
+  },
+  
+  count: function()
+  {
+    return Resolutions.find({complaintId: this.complaints._id}).count()
+  },
+  
+  resolutionText: function()
+  {
+    var count = Resolutions.find({complaintId: this.complaints._id}).count();
+    
+    if(count === 0) return "No Solutions Yet :/"
+   
+    var singular = count === 1;
+    
+    return "Good News! The following "+(!singular ? count : "")+" solution" + (!singular ? "s" : "") + " exist" + (!singular ? "" : "s") + ":"
   }
+  
+  
 })
 
 Template.complaintPage.events({
