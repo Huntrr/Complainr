b568d8a542fda965e94c71b85da09a12c9a5b760
Hunter Lightman <git.huntrr@gmail.com>
2015-09-05 00:31:38 -0400

    Added basic interface

--- Complainr.js
+++ Complainr.js
@@ -1,23 +1,47 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+Complaints = new Mongo.Collection("complaints");
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  // This code only runs on the server
+  Meteor.publish("complaints", function () {
+    return Complaints.find({ });
   });
 }
+
+if (Meteor.isClient) {
+  Meteor.subscribe("tasks");
+
+  // Defines the variables ({{VAR_NAME}}) in the html nody
+  Template.body.helpers({
+    complaints: function() {
+      return Complaints.find({});
+    }
+  });
+
+  // Defines the EVENTS bound to the SUBMISSION template
+  Template.submission.events({
+    "submit .new-complaint": function (e) {
+      e.preventDefault();
+
+      var title = e.target.title.value;
+      var desc = e.target.description.value;
+
+      Meteor.call("addComplaint", title, desc);
+
+      //Clear the form
+      e.target.title.value = "";
+      e.target.description.value = "";
+    }
+  });
+}
+
+
+//METHODS (these need to be defined here so they can be run on the server to verify they're valid
+Meteor.methods({
+  "addComplaint": function(title, desc) {
+    Complaints.insert({
+      title: title,
+      desc: desc,
+      createdAt: new Date(),
+    });
+  }
+});
