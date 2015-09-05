e0950309d22799b993230e71411d405b624701e5
dhruv9 <dhurvdabriwal@gmail.com>
2015-09-05 16:38:54 -0400

    Added Resolutions
    The layout is still pretty ugly tho

--- client/complaintPage.js
+++ client/complaintPage.js
@@ -1,5 +1,26 @@
+Template.complaintPage.helpers({
+  
+  resolutionList: function() {
+  return Resolutions.find({complaintId: this.complaints._id})
+  }
+})
+
 Template.complaintPage.events({
   "click .homeButton": function(e) {
     Router.go('home');
+  },
+  "submit .new-resolution": function(e) {
+    console.log("AWD");
+    e.preventDefault();
+
+    var desc = e.target.description.value;
+
+    Meteor.call("addResolution", this.complaints._id, desc);
+
+    //Clear the form
+    e.target.description.value = "";
   }
+    
+    
+    
 });
--- shared/methods.js
+++ shared/methods.js
@@ -1,4 +1,5 @@
 Complaints = new Mongo.Collection("complaints");
+Resolutions = new Mongo.Collection("resolutions");
 
 Meteor.methods({
   "addComplaint": function(title, desc) {
@@ -8,6 +9,14 @@ Meteor.methods({
       createdAt: new Date(),
       resolved: false,
       votes: 1
+    });
+  },
+  "addResolution": function(complaintId, description) {
+    Resolutions.insert({
+      complaintId: complaintId,
+      description: description,
+      createdAt: new Date(),
+      likes: 0
     });
   }
 });
